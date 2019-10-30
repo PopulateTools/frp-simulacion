8e25ad65c241a9e130872683eb13687c42e0a427
Jorge Aznar <jorgeatgu@gmail.com>
2019-10-30 15:03:30 +0100

    tables | tooltips | selector functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,40 @@
-function() {
-  console.table("it's a trap")
 
+document.querySelectorAll(".input-radio").forEach(input => input.addEventListener('click', getName));
+
+function getName() {
+  const targetElement = event.target;
+  const inputName = targetElement.getAttribute('name');
+  getActive(inputName)
 }
+
+function getActive(name){
+ const inputs = document.querySelectorAll(`input[name='${name}']`);
+  for(let i = 0; i < inputs.length; i++) {
+      const element = inputs[i].parentNode
+      if(inputs[i].checked === false) {
+        element.classList.remove('active-checkbox')
+      } else {
+        element.classList.add('active-checkbox')
+      }
+  }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    new Tooltip(document.getElementById("tooltip-pf"), {
+        placement: "top",
+        title: "La velocidad de consolidación es la velocidad en la que wadus wadus wadus. La velocidad de consolidación es la velocidad en la que wadus wadus wadus. La velocidad de consolidación es la velocidad en la que wadus wadus wadus. La velocidad de consolidación es la velocidad en la que wadus wadus wadus. ",
+    });
+    new Tooltip(document.getElementById("tooltip-idc"), {
+        placement: "top",
+        title: "La velocidad de consolidación es la velocidad en la que wadus wadus wadus. La velocidad de consolidación es la velocidad en la que wadus wadus wadus. La velocidad de consolidación es la velocidad en la que wadus wadus wadus. La velocidad de consolidación es la velocidad en la que wadus wadus wadus. ",
+    });
+    new Tooltip(document.getElementById("tooltip-vdc"), {
+        placement: "top",
+        title: "La velocidad de consolidación es la velocidad en la que wadus wadus wadus. La velocidad de consolidación es la velocidad en la que wadus wadus wadus. La velocidad de consolidación es la velocidad en la que wadus wadus wadus. La velocidad de consolidación es la velocidad en la que wadus wadus wadus. ",
+    });
+
+});
+
