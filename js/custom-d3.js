0f21d0763b985100877fa28045190912ea75408f
Hugoren Martinako <aumpfbahn@gmail.com>
2024-10-03 17:17:00 +0200

    Updates

--- js/custom-d3.js
+++ js/custom-d3.js
@@ -1,1 +1,148 @@
-import"d3-transition";export{select,selectAll,event,mouse,selection}from"d3-selection";export{min,max,extent}from"d3-array";export{scaleTime,scaleLinear,scaleBand,scaleOrdinal}from"d3-scale";export{keys,nest}from"d3-collection";export{line}from"d3-shape";export{axisBottom,axisLeft}from"d3-axis";export{csv}from"d3-request";export{easeLinear}from"d3-ease";export{format,formatDefaultLocale}from"d3-format";export{timeFormat}from"d3-time-format";
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "axisBottom", {
+  enumerable: true,
+  get: function get() {
+    return _d3Axis.axisBottom;
+  }
+});
+Object.defineProperty(exports, "axisLeft", {
+  enumerable: true,
+  get: function get() {
+    return _d3Axis.axisLeft;
+  }
+});
+Object.defineProperty(exports, "csv", {
+  enumerable: true,
+  get: function get() {
+    return _d3Request.csv;
+  }
+});
+Object.defineProperty(exports, "easeLinear", {
+  enumerable: true,
+  get: function get() {
+    return _d3Ease.easeLinear;
+  }
+});
+Object.defineProperty(exports, "event", {
+  enumerable: true,
+  get: function get() {
+    return _d3Selection.event;
+  }
+});
+Object.defineProperty(exports, "extent", {
+  enumerable: true,
+  get: function get() {
+    return _d3Array.extent;
+  }
+});
+Object.defineProperty(exports, "format", {
+  enumerable: true,
+  get: function get() {
+    return _d3Format.format;
+  }
+});
+Object.defineProperty(exports, "formatDefaultLocale", {
+  enumerable: true,
+  get: function get() {
+    return _d3Format.formatDefaultLocale;
+  }
+});
+Object.defineProperty(exports, "keys", {
+  enumerable: true,
+  get: function get() {
+    return _d3Collection.keys;
+  }
+});
+Object.defineProperty(exports, "line", {
+  enumerable: true,
+  get: function get() {
+    return _d3Shape.line;
+  }
+});
+Object.defineProperty(exports, "max", {
+  enumerable: true,
+  get: function get() {
+    return _d3Array.max;
+  }
+});
+Object.defineProperty(exports, "min", {
+  enumerable: true,
+  get: function get() {
+    return _d3Array.min;
+  }
+});
+Object.defineProperty(exports, "mouse", {
+  enumerable: true,
+  get: function get() {
+    return _d3Selection.mouse;
+  }
+});
+Object.defineProperty(exports, "nest", {
+  enumerable: true,
+  get: function get() {
+    return _d3Collection.nest;
+  }
+});
+Object.defineProperty(exports, "scaleBand", {
+  enumerable: true,
+  get: function get() {
+    return _d3Scale.scaleBand;
+  }
+});
+Object.defineProperty(exports, "scaleLinear", {
+  enumerable: true,
+  get: function get() {
+    return _d3Scale.scaleLinear;
+  }
+});
+Object.defineProperty(exports, "scaleOrdinal", {
+  enumerable: true,
+  get: function get() {
+    return _d3Scale.scaleOrdinal;
+  }
+});
+Object.defineProperty(exports, "scaleTime", {
+  enumerable: true,
+  get: function get() {
+    return _d3Scale.scaleTime;
+  }
+});
+Object.defineProperty(exports, "select", {
+  enumerable: true,
+  get: function get() {
+    return _d3Selection.select;
+  }
+});
+Object.defineProperty(exports, "selectAll", {
+  enumerable: true,
+  get: function get() {
+    return _d3Selection.selectAll;
+  }
+});
+Object.defineProperty(exports, "selection", {
+  enumerable: true,
+  get: function get() {
+    return _d3Selection.selection;
+  }
+});
+Object.defineProperty(exports, "timeFormat", {
+  enumerable: true,
+  get: function get() {
+    return _d3TimeFormat.timeFormat;
+  }
+});
+require("d3-transition");
+var _d3Selection = require("d3-selection");
+var _d3Array = require("d3-array");
+var _d3Scale = require("d3-scale");
+var _d3Collection = require("d3-collection");
+var _d3Shape = require("d3-shape");
+var _d3Axis = require("d3-axis");
+var _d3Request = require("d3-request");
+var _d3Ease = require("d3-ease");
+var _d3Format = require("d3-format");
+var _d3TimeFormat = require("d3-time-format");
